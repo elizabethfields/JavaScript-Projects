949c8958bf0cc035849fbc726b9c91935e4a705a
elizabethfields <elifields22@gmail.com>
2021-01-20 15:46:57 -0700

    project 1 expressions
    project 1 expressions

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,16 @@
+window.alert("Hello, World!"); // window alert //
+const quote = "Hello,World!"; // first variable //
+const speaker = "Steve Jobs"; //second variable //
+const saying = quote  +  " was first coined by " + speaker  +  " as the first sentence ever to be displayed on a computer screen but some say that Brian Kernihan was the first to record this famous phrase as part of his documentation for the BCPL programming language developed by Martin Richards.  "; // Assign string values to two variables and concatenate the variables together //
+document.write(saying); // string values + two variables display //
+function My_First_Function() { 
+    var str = "Learn More";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result; //button + font color changes when clicked//
+}
+document.write("Below is an example of an expression <br></br>");
+document.write(3 +3); //expression//
+
+
+
+
