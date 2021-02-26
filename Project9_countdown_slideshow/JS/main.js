56d3d328665308e57ca0dc83b673d9be2ce71d4b
elizabethfields <elifields22@gmail.com>
2021-02-26 16:51:34 -0700

    final
    final

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -12,11 +12,17 @@ function countdown() {
 tick();
 }
 
-var i = 0;
-function change_image();
-document.getElementById("slideshow").src = "img" + (i++ % 6) +"
-    .jpg";
-}
-function slider() {
-    setInterval(change_image, 1000);
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
